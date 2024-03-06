f316a72e23e2b3d1952b277d735ae3edc53118b6
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-03-06 13:27:48 +0530

    commit changes all files

---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react";
+import { DarkThemeToggle, Navbar } from "flowbite-react";
+
+const ExampleNavbar: FC = function () {
+  return (
+    <Navbar fluid>
+      <div className="w-full p-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">Admin dashboard</div>
+          <div className="flex items-center gap-3">
+            <DarkThemeToggle />
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  );
+};
+
+export default ExampleNavbar;
