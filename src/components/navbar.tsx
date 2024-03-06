a01dbfbbfc498a32293c10d678f09c547356f6dc
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-03-06 22:47:35 +0530

    commit changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import type { FC } from "react";
 import { DarkThemeToggle, Navbar } from "flowbite-react";
 // import SignUpPage from "../pages/authentication/sign-up";
 
-
 // main header is there
 const ExampleNavbar: FC = function () {
+  // const [open, setOpen ]=useState()
   return (
-    <Navbar fluid>
-      <div className="w-full p-3 lg:px-5 lg:pl-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">Admin dashboard</div>
-          <div className="flex items-center gap-3">
-            <DarkThemeToggle />
+    <>
+      <Navbar fluid>
+        <div className="w-full p-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">Admin dashboard</div>
+            <div className="flex items-center gap-3">
+              <DarkThemeToggle />
+            </div>
           </div>
         </div>
-      </div>
-    </Navbar>
+      </Navbar>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ import { Flowbite } from "flowbite-react";
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import DashboardPage from "./pages";
-import SignInPage from "./pages/authentication/sign-in";
-import SignUpPage from "./pages/authentication/sign-up";
 import EcommerceProductsPage from "./pages/e-commerce/products";
 import UserListPage from "./pages/users/list";
 
@@ -25,9 +23,7 @@ root.render(
     <Flowbite theme={{ theme }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DashboardPage />} index />
-          <Route path="/authentication/sign-in" element={<SignInPage />} />
-          <Route path="/authentication/sign-up" element={<SignUpPage />} />
+          <Route path="/" element={<DashboardPage />} />
           <Route
             path="/e-commerce/products"
             element={<EcommerceProductsPage />}
